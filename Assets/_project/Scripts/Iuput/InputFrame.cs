cf602f9ed0f2a3d45d7e776d2ab5beb24623beec
Smokie889 <a0978432963@gmail.com>
2026-04-13 17:38:56 +0800

    init unity 1slash client

---
+++ Assets/_project/Scripts/Iuput/InputFrame.cs
@@ -0,0 +1,11 @@
+namespace OneSlash.Client.Input
+{
+    [System.Serializable]
+    public struct InputFrame
+    {
+        public float MoveX;
+        public bool ChargeHeld;
+        public bool AttackPressed;
+        public bool ParryPressed;
+    }
+}
